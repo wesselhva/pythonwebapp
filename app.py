15470e331f798cb395351ab33a1230d6316ab3b8
fakawes <wessel.rooth@gmail.com>
2022-05-18 08:06:54 +0200

    piw

--- app.py
+++ app.py
@@ -1,111 +1,14 @@
-from distutils.log import debug
-import uuid
-import requests
-from flask import Flask, render_template, session, request, redirect, url_for
-from flask_session import Session  # https://pythonhosted.org/Flask-Session
-import msal
-import app_config
-from werkzeug.middleware.proxy_fix import ProxyFix
-from service_bus import send_to_servicebus,get_from_servicebus
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object(app_config)
-debug=True
-app.config["TEMPLATES_AUTO_RELOAD"]= True
-Session(app)
-
-
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 @app.route("/")
-def index():
-    if not session.get("user"):
-        return redirect(url_for("login"))
-    return render_template('index.html', user=session["user"], version=msal.__version__)
+def home():
+    return render_template("index.html")
 
-@app.route("/login")
-def login():
-    session["flow"] = _build_auth_code_flow(scopes=app_config.SCOPE)
-    return render_template("login.html", auth_url=session["flow"]["auth_uri"], version=msal.__version__)
-
-@app.route(app_config.REDIRECT_PATH)  # Applicatie URI 
-def authorized():
-    try:
-        cache = _load_cache()
-        result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
-            session.get("flow", {}), request.args)
-        if "error" in result:
-            return render_template("auth_error.html", result=result)
-        session["user"] = result.get("id_token_claims")
-        _save_cache(cache)
-    except ValueError:
-        pass
-    return redirect(url_for("index"))
-
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(
-        app_config.AUTHORITY + "/oauth2/v2.0/logout" +
-        "?post_logout_redirect_uri=" + url_for("index", _external=True))
-
-@app.route("/graphcall")
-def graphcall():
-    token = _get_token_from_cache(app_config.SCOPE)
-    if not token:
-        return redirect(url_for("login"))
-    graph_data = requests.get(  # Use token to call downstream service
-        app_config.CENDPOINT,
-        headers={'Authorization': 'Bearer ' + token['access_token']},
-        ).json()['value']   
-    return render_template('schedule.html', data=graph_data)
-    # agendaTitel = graph_data['bodyPreview']
-    # send_to_servicebus(agendaTitel)
-    # service_bus_data = get_from_servicebus()
-    # print(service_bus_data)
-
-@app.route("/egraphcall")
-def egraphcall():
-    token = _get_token_from_cache(app_config.SCOPE)
-    if not token:
-        return redirect(url_for("login"))
-    graph_data = requests.get(
-        app_config.EENDPOINT,
-        headers={'Authorization': 'Bearer ' + token['access_token']},
-        ).json()
-    return render_template('display.html', result=graph_data)
-
-def _load_cache():
-    cache = msal.SerializableTokenCache()
-    if session.get("token_cache"):
-        cache.deserialize(session["token_cache"])
-    return cache
-
-def _save_cache(cache):
-    if cache.has_state_changed:
-        session["token_cache"] = cache.serialize()
-
-def _build_msal_app(cache=None, authority=None):
-    return msal.ConfidentialClientApplication(
-        app_config.CLIENT_ID, authority=authority or app_config.AUTHORITY,
-        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
-
-def _build_auth_code_flow(authority=None, scopes=None):
-    return _build_msal_app(authority=authority).initiate_auth_code_flow(
-        scopes or [],
-        redirect_uri=url_for("authorized", _external=True))
-
-def _get_token_from_cache(scope=None):
-    cache = _load_cache()
-    cca = _build_msal_app(cache=cache)
-    accounts = cca.get_accounts()
-    if accounts:
-        result = cca.acquire_token_silent(scope, account=accounts[0])
-        _save_cache(cache)
-        return result
-
-app.jinja_env.globals.update(_build_auth_code_flow=_build_auth_code_flow)  # Used in template
-
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
 if __name__ == "__main__":
-    app.run()
-
+    app.run(debug=True)
